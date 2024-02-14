fc90ef3dbd8773af0fcf0b94e986d691bec46278
aetos <aetos382@users.noreply.github.com>
2024-02-14 18:43:21 +0900

    add benchmark
    <UseArtifactsOutput> は BenchmarkDotnet と互換性がないので一時的に無効化。
    BenchmarkDotnet 0.14 で修正されるらしい。

---
+++ ToKanji.Benchmark/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using ToKanjiLib;
+
+BenchmarkRunner.Run<ToKanjiBenchmark>();
+
+[ShortRunJob]
+[MemoryDiagnoser]
+public class ToKanjiBenchmark
+{
+    [GlobalSetup]
+    public static void GlobalSetup()
+    {
+        Console.WriteLine("GlobalSetup");
+    }
+
+    [Benchmark]
+    public int ToKanji()
+    {
+        Span<char> buffer = stackalloc char[50];
+
+        var filledChars = Converter.ToKanji(1234_5678_9012_3456, buffer);
+
+        return filledChars;
+    }
+}
