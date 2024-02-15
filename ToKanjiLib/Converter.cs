17985aa04526920d9a1a2e34f07f0af0ecac389a
aetos <aetos382@users.noreply.github.com>
2024-02-15 13:08:19 +0900

    wip

--- ToKanjiLib/Converter.cs
+++ ToKanjiLib/Converter.cs
@@ -26,6 +26,8 @@ public static class Converter
             throw new NotSupportedException();
         }
 
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(number, ValueHolder<T>.MaxValue);
+
         if (number == T.Zero)
         {
             destination[0] = '〇';
@@ -42,18 +44,7 @@ public static class Converter
             14, 15, 15, 15, 15, 16, 16, 16, 17, 17, 17, 18, 18, 18, 18, 19
         ];
 
-        var q = ValueHolder<T>.MaxPowerOf10;
-        var p = new List<T>(q);
-        var v = T.One;
-
-        for (int i = 0; i < q; ++i)
-        {
-            p.Add(v);
-
-            v *= ValueHolder<T>.Ten;
-        }
-
-        ReadOnlySpan<T> powersOf10 = CollectionsMarshal.AsSpan(p);
+        var powersOf10 = ValueHolder<T>.PowersOf10;
 
         ReadOnlySpan<char> digits = ['〇', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
         ReadOnlySpan<char> units1 = ['一', '十', '百', '千'];
@@ -138,84 +129,59 @@ public static class Converter
         public static bool Supported;
         public static T Ten;
         public static T MaxValue;
-        public static int MaxPowerOf10;
+        public static uint MaxPowerOf10;
+
+        public static ReadOnlySpan<T> PowersOf10
+        {
+            get
+            {
+                return CollectionsMarshal.AsSpan(_powersOf10);
+            }
+        }
+
+        private static List<T> _powersOf10;
+
+        public static void Initialize(
+            T ten,
+            T maxValue,
+            uint maxPowerOf10)
+        {
+            Supported = true;
+            Ten = ten;
+            MaxValue = maxValue;
+            MaxPowerOf10 = maxPowerOf10;
+
+            var p = new List<T>(unchecked((int)maxPowerOf10));
+            var v = T.One;
+
+            for (var i = 0; i < maxPowerOf10; ++i)
+            {
+                p.Add(v);
+                v *= ten;
+            }
+
+            _powersOf10 = p;
+        }
     }
 
     static Converter()
     {
-        ValueHolder<byte>.Supported = true;
-        ValueHolder<byte>.Ten = 10;
-        ValueHolder<byte>.MaxValue = byte.MaxValue;
-        ValueHolder<byte>.MaxPowerOf10 = 2;
+        ValueHolder<byte>.Initialize(10, byte.MaxValue, 2);
+        ValueHolder<sbyte>.Initialize(10, sbyte.MaxValue, 2);
+        ValueHolder<char>.Initialize((char)10, char.MaxValue, 4);
+        ValueHolder<short>.Initialize(10, short.MaxValue, 4);
+        ValueHolder<ushort>.Initialize(10, ushort.MaxValue, 4);
+        ValueHolder<int>.Initialize(10, int.MaxValue, 9);
+        ValueHolder<uint>.Initialize(10, uint.MaxValue, 9);
+        ValueHolder<long>.Initialize(10, long.MaxValue, 19);
+        ValueHolder<ulong>.Initialize(10, ulong.MaxValue, 19);
+        ValueHolder<nint>.Initialize(10, nint.MaxValue, Environment.Is64BitProcess ? 19U : 9U);
+        ValueHolder<nuint>.Initialize(10, nuint.MaxValue, Environment.Is64BitProcess ? 19U : 9U);
+        ValueHolder<Int128>.Initialize(10, Int128.MaxValue, 38);
+        ValueHolder<UInt128>.Initialize(10, UInt128.MaxValue, 38);
 
-        ValueHolder<sbyte>.Supported = true;
-        ValueHolder<sbyte>.Ten = 10;
-        ValueHolder<sbyte>.MaxValue = sbyte.MaxValue;
-        ValueHolder<sbyte>.MaxPowerOf10 = 2;
-
-        ValueHolder<char>.Supported = true;
-        ValueHolder<char>.Ten = (char)10;
-        ValueHolder<char>.MaxValue = char.MaxValue;
-        ValueHolder<char>.MaxPowerOf10 = 4;
-
-        ValueHolder<short>.Supported = true;
-        ValueHolder<short>.Ten = 10;
-        ValueHolder<short>.MaxValue = short.MaxValue;
-        ValueHolder<short>.MaxPowerOf10 = 4;
-
-        ValueHolder<ushort>.Supported = true;
-        ValueHolder<ushort>.Ten = 10;
-        ValueHolder<ushort>.MaxValue = ushort.MaxValue;
-        ValueHolder<ushort>.MaxPowerOf10 = 4;
-
-        ValueHolder<int>.Supported = true;
-        ValueHolder<int>.Ten = 10;
-        ValueHolder<int>.MaxValue = int.MaxValue;
-        ValueHolder<int>.MaxPowerOf10 = 9;
-
-        ValueHolder<uint>.Supported = true;
-        ValueHolder<uint>.Ten = 10;
-        ValueHolder<uint>.MaxValue = uint.MaxValue;
-        ValueHolder<uint>.MaxPowerOf10 = 9;
-
-        ValueHolder<long>.Supported = true;
-        ValueHolder<long>.Ten = 10;
-        ValueHolder<long>.MaxValue = long.MaxValue;
-        ValueHolder<long>.MaxPowerOf10 = 19;
-
-        ValueHolder<ulong>.Supported = true;
-        ValueHolder<ulong>.Ten = 10;
-        ValueHolder<ulong>.MaxValue = ulong.MaxValue;
-        ValueHolder<ulong>.MaxPowerOf10 = 19;
-
-        ValueHolder<nint>.Supported = true;
-        ValueHolder<nint>.Ten = 10;
-        ValueHolder<nint>.MaxValue = nint.MaxValue;
-        ValueHolder<nint>.MaxPowerOf10 = Environment.Is64BitProcess
-            ? ValueHolder<long>.MaxPowerOf10
-            : ValueHolder<int>.MaxPowerOf10;
-
-        ValueHolder<nuint>.Supported = true;
-        ValueHolder<nuint>.Ten = 10;
-        ValueHolder<nuint>.MaxValue = nuint.MaxValue;
-        ValueHolder<nint>.MaxPowerOf10 = Environment.Is64BitProcess
-            ? ValueHolder<ulong>.MaxPowerOf10
-            : ValueHolder<uint>.MaxPowerOf10;
-
-        ValueHolder<Int128>.Supported = true;
-        ValueHolder<Int128>.Ten = 10;
-        ValueHolder<Int128>.MaxValue = Int128.MaxValue;
-        ValueHolder<Int128>.MaxPowerOf10 = 38;
-
-        ValueHolder<UInt128>.Supported = true;
-        ValueHolder<UInt128>.Ten = 10;
-        ValueHolder<UInt128>.MaxValue = UInt128.MaxValue;
-        ValueHolder<UInt128>.MaxPowerOf10 = 38;
-
-        ValueHolder<BigInteger>.Supported = true;
-        ValueHolder<BigInteger>.Ten = 10;
-        ValueHolder<BigInteger>.MaxValue = UInt128.MaxValue;
-        ValueHolder<BigInteger>.MaxPowerOf10 = 68;
+        // TODO: 暫定
+        ValueHolder<BigInteger>.Initialize(10, UInt128.MaxValue, 38);
     }
 
     public static int ToKanjiX(
